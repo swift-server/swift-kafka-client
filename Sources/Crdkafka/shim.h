67674ab47d0c4fc5ab4e96a30412251ae6be18d5
Felix Schlegel <fefefe152@gmail.com>
2022-07-21 19:09:17 +0200

    Changed Docker Ubuntu version + import librdkafka using system header
    Motivation:
    
    These changes were proposed in the Pull Request #15.
    
    Modifications:
    
    * Ubuntu version bionic -> focal
    * removed platform flag for Docker base image
    * changed import of librdkafka to system header (#include <>)

--- Sources/Crdkafka/shim.h
+++ Sources/Crdkafka/shim.h
@@ -1,8 +1,4 @@
 #ifndef __RD_KAFKA_SHIM_H__
 #define __RD_KAFKA_SHIM_H__
-#ifdef __APPLE__
-#include "/usr/local/include/librdkafka/rdkafka.h"
-#else
-#include "/usr/include/librdkafka/rdkafka.h"
-#endif
+#include <librdkafka/rdkafka.h>
 #endif
