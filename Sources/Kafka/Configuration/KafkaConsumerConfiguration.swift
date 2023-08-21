2be2bd9d958ffe9bcfc93213a08b45253fcd0eb9
blindspotbounty <127803250+blindspotbounty@users.noreply.github.com>
2023-08-21 11:00:55 +0300

    Update Sources/Kafka/Configuration/KafkaConsumerConfiguration.swift
    Co-authored-by: Felix Schlegel <fefefe152@gmail.com>

--- Sources/Kafka/Configuration/KafkaConsumerConfiguration.swift
+++ Sources/Kafka/Configuration/KafkaConsumerConfiguration.swift
@@ -208,7 +208,7 @@ public struct KafkaConsumerConfiguration {
     public var reconnect: KafkaConfiguration.ReconnectOptions = .init()
 
     /// Options for librdkafka metrics updates
-    public var metrics: KafkaConfiguration.Metrics = .disable
+    public var metrics: KafkaConfiguration.Metrics = .disabled
 
     /// Security protocol to use (plaintext, ssl, sasl_plaintext, sasl_ssl).
     /// Default: `.plaintext`
