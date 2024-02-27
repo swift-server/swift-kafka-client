01779edae1b2d58ac2574a4caebcae6823cc15d7
BlindSpot <127803250+blindspotbounty@users.noreply.github.com>
2024-02-27 12:16:26 +0300

    add header

--- dev/test-benchmark-thresholds.sh
+++ dev/test-benchmark-thresholds.sh
@@ -1,3 +1,18 @@
+#!/bin/bash
+##===----------------------------------------------------------------------===##
+##
+## This source file is part of the swift-kafka-client open source project
+##
+## Copyright (c) YEARS Apple Inc. and the swift-kafka-client project authors
+## Licensed under Apache License v2.0
+##
+## See LICENSE.txt for license information
+## See CONTRIBUTORS.txt for the list of swift-kafka-client project authors
+##
+## SPDX-License-Identifier: Apache-2.0
+##
+##===----------------------------------------------------------------------===##
+
 cd Benchmarks
 swift package --disable-sandbox benchmark baseline update PR --no-progress
 git checkout main
