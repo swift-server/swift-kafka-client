9ae2a6bd5fcef83b9ff996a38591da1a43de8e38
Rick Newton-Rogers <rnro@apple.com>
2025-05-13 09:30:49 +0100

    Drop Swift 5.9
    Motivation:
    
    Swift 5.9 is no longer supported, we should bump the tools version and remove it from our CI.
    
    Modifications:
    
    * Bump the Swift tools version to Swift 5.10
    * Remove Swift 5.9 jobs where appropriate in main.yml, pull_request.yml
    
    Result:
    
    Code reflects our support window.

--- Benchmarks/Package.swift
+++ Benchmarks/Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 5.7
+// swift-tools-version:5.10
 //===----------------------------------------------------------------------===//
 //
 // This source file is part of the swift-kafka-client open source project
