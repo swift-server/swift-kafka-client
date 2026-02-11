45ec7c638ee5e5420d8914582e73850d9f6fbae6
Alexey Bezhan <alexeyb@apple.com>
2026-02-11 15:22:14 +0000

    Fix early end of sequence bug in `TaskExecutor` poll implementation
    If there are no new messages available within the `pollInterval` we
    used to return `nil`, which signaled the end of the sequence.
    
    Now we only return if we get a message from `.consumerPoll`, otherwise
    we continue with the outer `while` loop.

--- Sources/Kafka/KafkaConsumer.swift
+++ Sources/Kafka/KafkaConsumer.swift
@@ -110,8 +110,11 @@ public struct KafkaConsumerMessages: Sendable, AsyncSequence {
                     if #available(macOS 15.0, iOS 18.0, watchOS 11.0, tvOS 18.0, visionOS 2.0, *) {
                         // Wait on a separate thread for the next message.
                         // The call below will block for `pollInterval`.
-                        return try await withTaskExecutorPreference(queue) {
-                            try client.consumerPoll(for: Int32(self.pollInterval.inMilliseconds))
+                        if let message = try await withTaskExecutorPreference(
+                            queue,
+                            operation: { try client.consumerPoll(for: Int32(self.pollInterval.inMilliseconds)) }
+                        ) {
+                            return message
                         }
                     } else {
                         // No messages. Sleep a little.
