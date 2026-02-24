b3dd47fb7b86a64cbd2af6dc1c2e1f0c0e9fdf60
Alexey Bezhan <alexeyb@apple.com>
2026-02-24 10:51:43 +0000

    Convert integration tests to Swift Testing (#210)
    ### Motivation
    
    Tests should use the latest Swift Testing framework when possible.
    Converted tests now run in parallel by default.
    
    ### Modifications
    
    #### Convert integration tests to Swift Testing
    
    Swift Testing doesn't allow `async throws` in teardown logic so we
    extract topic creation into a `withTestTopic` helper function.
    
    This can be replaced with a custom test scoping trait when we only
    support Swift >6.1.
    
    #### Make topic creation and deletion non-blocking
    
    Currently we block for up to 10s waiting for the result of topic
    creation or deletion.
    
    Instead, we register a callback with librdkafka to be notified when a
    result event is available. We then suspend the task until callback
    resumes the continuation.
    
    #### Add `.produceMessages` integration tests helper
    
    Reduce the duplication in integration tests code by making it easier to
    setup a topic with messages.
    
    Running with Kafka 3.9 in KRaft mode the first topic created after Kafka
    cluster start might not be available for the subscribe call since
    `__consumer_offsets` internal topic is being auto-created at the same
    time.
    
    Having the producer access the topic first addresses this without
    requiring additional checks or delays in test setup.
    
    #### Fix consumer rebalance integration test
    
    `_createTestMessages` helper used the same key for all messages, which
    means that with the default `consistent_random` partitioner they all
    land on the same partition.
    
    `duplicatedMessagesOnRebalance` test then takes between 30 and ~110
    seconds to run depending on whether that partition gets picked up by
    consumer 1 or 2 after the rebalance.
    
    This switches to using a random UUID for test message keys and replaces
    long static delays from the test with per-consumer message counters that
    are used to verify that the rebalance actually happened.
    
    The test now takes 3-5 seconds to run and makes sure both consumers are
    receiving messages.
    
    ### Result
    
    Integration tests take ~10s to run compared to 2-3 minutes.

--- Sources/Kafka/ForTesting/RDKafkaClient+Topic.swift
+++ Sources/Kafka/ForTesting/RDKafkaClient+Topic.swift
@@ -17,15 +17,20 @@ import Logging
 
 import struct Foundation.UUID
 
+private func queueEventCallback(_ rk: OpaquePointer?, _ context: UnsafeMutableRawPointer?) {
+    guard let context = context else { return }
+    let continuation =
+        Unmanaged<AnyObject>.fromOpaque(context).takeRetainedValue() as! CheckedContinuation<Void, Never>
+    continuation.resume()
+}
+
 @_spi(Internal)
 extension RDKafkaClient {
     /// Create a topic with a unique name (`UUID`).
-    /// Blocks for a maximum of `timeout` milliseconds.
     /// - Parameter partitions: Partitions in topic (default: -1 - default for broker)
-    /// - Parameter timeout: Timeout in milliseconds.
     /// - Returns: Name of newly created topic.
     /// - Throws: A ``KafkaError`` if the topic creation failed.
-    public func _createUniqueTopic(partitions: Int32 = -1, timeout: Int32) throws -> String {
+    public func _createUniqueTopic(partitions: Int32 = -1) async throws -> String {
         let uniqueTopicName = UUID().uuidString
 
         let errorChars = UnsafeMutablePointer<CChar>.allocate(capacity: RDKafkaClient.stringSize)
@@ -45,116 +50,141 @@ extension RDKafkaClient {
         }
         defer { rd_kafka_NewTopic_destroy(newTopic) }
 
-        try self.withKafkaHandlePointer { kafkaHandle in
-            let resultQueue = rd_kafka_queue_new(kafkaHandle)
-            defer { rd_kafka_queue_destroy(resultQueue) }
+        let resultQueue = self.withKafkaHandlePointer { kafkaHandle in
+            rd_kafka_queue_new(kafkaHandle)
+        }
+        defer { rd_kafka_queue_destroy(resultQueue) }
 
-            var newTopicsArray: [OpaquePointer?] = [newTopic]
-            rd_kafka_CreateTopics(
-                kafkaHandle,
-                &newTopicsArray,
-                1,
-                nil,
-                resultQueue
+        await withCheckedContinuation { (continuation: CheckedContinuation<Void, Never>) in
+            rd_kafka_queue_cb_event_enable(
+                resultQueue,
+                queueEventCallback,
+                Unmanaged.passRetained(continuation as AnyObject).toOpaque()
             )
 
-            guard let resultEvent = rd_kafka_queue_poll(resultQueue, timeout) else {
-                throw KafkaError.topicCreation(reason: "No CreateTopics result after 10s timeout")
-            }
-            defer { rd_kafka_event_destroy(resultEvent) }
-
-            let resultCode = rd_kafka_event_error(resultEvent)
-            guard resultCode == RD_KAFKA_RESP_ERR_NO_ERROR else {
-                throw KafkaError.rdKafkaError(wrapping: resultCode)
-            }
-
-            guard let topicsResultEvent = rd_kafka_event_CreateTopics_result(resultEvent) else {
-                throw KafkaError.topicCreation(
-                    reason: "Received event that is not of type rd_kafka_CreateTopics_result_t"
+            self.withKafkaHandlePointer { kafkaHandle in
+                var newTopicsArray: [OpaquePointer?] = [newTopic]
+                rd_kafka_CreateTopics(
+                    kafkaHandle,
+                    &newTopicsArray,
+                    1,
+                    nil,
+                    resultQueue
                 )
             }
+        }
 
-            var resultTopicCount = 0
-            let topicResults = rd_kafka_CreateTopics_result_topics(
-                topicsResultEvent,
-                &resultTopicCount
+        guard let resultEvent = rd_kafka_queue_poll(resultQueue, 0) else {
+            throw KafkaError.topicCreation(reason: "No CreateTopics result event")
+        }
+        defer { rd_kafka_event_destroy(resultEvent) }
+
+        let resultCode = rd_kafka_event_error(resultEvent)
+        guard resultCode == RD_KAFKA_RESP_ERR_NO_ERROR else {
+            throw KafkaError.rdKafkaError(wrapping: resultCode)
+        }
+
+        guard let topicsResultEvent = rd_kafka_event_CreateTopics_result(resultEvent) else {
+            throw KafkaError.topicCreation(
+                reason: "Received event that is not of type rd_kafka_CreateTopics_result_t"
             )
+        }
 
-            guard resultTopicCount == 1, let topicResult = topicResults?[0] else {
-                throw KafkaError.topicCreation(reason: "Received less/more than one topic result")
-            }
+        var resultTopicCount = 0
+        let topicResults = rd_kafka_CreateTopics_result_topics(
+            topicsResultEvent,
+            &resultTopicCount
+        )
 
-            let topicResultError = rd_kafka_topic_result_error(topicResult)
-            guard topicResultError == RD_KAFKA_RESP_ERR_NO_ERROR else {
-                throw KafkaError.rdKafkaError(wrapping: topicResultError)
-            }
+        guard resultTopicCount == 1, let topicResult = topicResults?[0] else {
+            throw KafkaError.topicCreation(reason: "Received less/more than one topic result")
+        }
 
-            let receivedTopicName = String(cString: rd_kafka_topic_result_name(topicResult))
-            guard receivedTopicName == uniqueTopicName else {
-                throw KafkaError.topicCreation(reason: "Received topic result for topic with different name")
-            }
+        let topicResultError = rd_kafka_topic_result_error(topicResult)
+        guard topicResultError == RD_KAFKA_RESP_ERR_NO_ERROR else {
+            throw KafkaError.rdKafkaError(wrapping: topicResultError)
+        }
+
+        let receivedTopicName = String(cString: rd_kafka_topic_result_name(topicResult))
+        guard receivedTopicName == uniqueTopicName else {
+            throw KafkaError.topicCreation(
+                reason: "Received topic result for topic with different name"
+            )
         }
 
         return uniqueTopicName
     }
 
     /// Delete a topic.
-    /// Blocks for a maximum of `timeout` milliseconds.
     /// - Parameter topic: Topic to delete.
-    /// - Parameter timeout: Timeout in milliseconds.
     /// - Throws: A ``KafkaError`` if the topic deletion failed.
-    public func _deleteTopic(_ topic: String, timeout: Int32) throws {
+    public func _deleteTopic(_ topic: String) async throws {
         let deleteTopic = rd_kafka_DeleteTopic_new(topic)
         defer { rd_kafka_DeleteTopic_destroy(deleteTopic) }
 
-        try self.withKafkaHandlePointer { kafkaHandle in
-            let resultQueue = rd_kafka_queue_new(kafkaHandle)
-            defer { rd_kafka_queue_destroy(resultQueue) }
+        let resultQueue = try self.withKafkaHandlePointer { kafkaHandle in
+            guard let queue = rd_kafka_queue_new(kafkaHandle) else {
+                throw KafkaError.topicDeletion(reason: "Failed to create result queue")
+            }
+            return queue
+        }
+        defer { rd_kafka_queue_destroy(resultQueue) }
 
-            var deleteTopicsArray: [OpaquePointer?] = [deleteTopic]
-            rd_kafka_DeleteTopics(
-                kafkaHandle,
-                &deleteTopicsArray,
-                1,
-                nil,
-                resultQueue
+        await withCheckedContinuation { (continuation: CheckedContinuation<Void, Never>) in
+            rd_kafka_queue_cb_event_enable(
+                resultQueue,
+                queueEventCallback,
+                Unmanaged.passRetained(continuation as AnyObject).toOpaque()
             )
 
-            guard let resultEvent = rd_kafka_queue_poll(resultQueue, timeout) else {
-                throw KafkaError.topicDeletion(reason: "No DeleteTopics result after 10s timeout")
-            }
-            defer { rd_kafka_event_destroy(resultEvent) }
-
-            let resultCode = rd_kafka_event_error(resultEvent)
-            guard resultCode == RD_KAFKA_RESP_ERR_NO_ERROR else {
-                throw KafkaError.rdKafkaError(wrapping: resultCode)
-            }
-
-            guard let topicsResultEvent = rd_kafka_event_DeleteTopics_result(resultEvent) else {
-                throw KafkaError.topicDeletion(
-                    reason: "Received event that is not of type rd_kafka_DeleteTopics_result_t"
+            self.withKafkaHandlePointer { kafkaHandle in
+                var deleteTopicsArray: [OpaquePointer?] = [deleteTopic]
+                rd_kafka_DeleteTopics(
+                    kafkaHandle,
+                    &deleteTopicsArray,
+                    1,
+                    nil,
+                    resultQueue
                 )
             }
+        }
 
-            var resultTopicCount = 0
-            let topicResults = rd_kafka_DeleteTopics_result_topics(
-                topicsResultEvent,
-                &resultTopicCount
+        guard let resultEvent = rd_kafka_queue_poll(resultQueue, 0) else {
+            throw KafkaError.topicDeletion(reason: "No DeleteTopics result event")
+        }
+        defer { rd_kafka_event_destroy(resultEvent) }
+
+        let resultCode = rd_kafka_event_error(resultEvent)
+        guard resultCode == RD_KAFKA_RESP_ERR_NO_ERROR else {
+            throw KafkaError.rdKafkaError(wrapping: resultCode)
+        }
+
+        guard let topicsResultEvent = rd_kafka_event_DeleteTopics_result(resultEvent) else {
+            throw KafkaError.topicDeletion(
+                reason: "Received event that is not of type rd_kafka_DeleteTopics_result_t"
             )
+        }
 
-            guard resultTopicCount == 1, let topicResult = topicResults?[0] else {
-                throw KafkaError.topicDeletion(reason: "Received less/more than one topic result")
-            }
+        var resultTopicCount = 0
+        let topicResults = rd_kafka_DeleteTopics_result_topics(
+            topicsResultEvent,
+            &resultTopicCount
+        )
 
-            let topicResultError = rd_kafka_topic_result_error(topicResult)
-            guard topicResultError == RD_KAFKA_RESP_ERR_NO_ERROR else {
-                throw KafkaError.rdKafkaError(wrapping: topicResultError)
-            }
+        guard resultTopicCount == 1, let topicResult = topicResults?[0] else {
+            throw KafkaError.topicDeletion(reason: "Received less/more than one topic result")
+        }
 
-            let receivedTopicName = String(cString: rd_kafka_topic_result_name(topicResult))
-            guard receivedTopicName == topic else {
-                throw KafkaError.topicDeletion(reason: "Received topic result for topic with different name")
-            }
+        let topicResultError = rd_kafka_topic_result_error(topicResult)
+        guard topicResultError == RD_KAFKA_RESP_ERR_NO_ERROR else {
+            throw KafkaError.rdKafkaError(wrapping: topicResultError)
+        }
+
+        let receivedTopicName = String(cString: rd_kafka_topic_result_name(topicResult))
+        guard receivedTopicName == topic else {
+            throw KafkaError.topicDeletion(
+                reason: "Received topic result for topic with different name"
+            )
         }
     }
 
